6ed239274cac28d63ea1505a69baf7ebacc4c438
Denis Gruia <denis.gruiax@icloud.com>
2023-08-30 03:33:49 +0300

    The problem was not in the displaying the candlesticks, was in get_candlesticks function, because of the tail recursive implementation that returns reversed order after processing the data from json file.

--- lib/klines/klines.ml
+++ lib/klines/klines.ml
@@ -101,7 +101,7 @@ module Make(P : Parameters) : CandleStick = struct
 
   let parse_kline_data json  = let rec parse_kline_data' json acc = match json with
       |`A (`A head :: tail) -> parse_kline_data' (`A tail) ((get_data head) :: acc)
-      |_ -> acc 
+      |_ -> List.rev acc 
     in parse_kline_data' json [];;
 
   let get_candlesticks () = parse_kline_data (get_json_from_api_url endpoint);;
