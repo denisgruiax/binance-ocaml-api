5a4779d0feb510dfa8a0b21031efe8f0bb9036b6
Denis Gruia <denis.gruiax@icloud.com>
2023-09-20 17:09:12 +0300

    Update the structure of unit test modules to be in mirror with the implementation structure.

--- test/binance_ocaml_api_test.ml
+++ test/binance_ocaml_api_test.ml
@@ -3,9 +3,11 @@ include Binance_ocaml_api;;
 let library_unit_testing () =
   let open Alcotest in run "Module Endoints" [
     Utilities_test.suite ();
-    Symbol_price_ticker_test.suite ();
-    Klines_test.suite ();
-    New_order_test.suite ()
+    
+    Market_data_endpoints_test.Symbol_price_ticker_test.suite ();
+    Market_data_endpoints_test.Klines_test.suite ();
+
+    Spot_account_test.New_order_test.suite ()
   ];;
 
 library_unit_testing ();;
---
+++ test/market_data_endpoints/market_data_endpoints_test.ml
@@ -0,0 +1,2 @@
+module Klines_test = Klines_test
+module Symbol_price_ticker_test = Symbol_price_ticker_test
