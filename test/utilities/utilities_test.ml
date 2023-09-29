98d50f52cb45ff88a0cdf903ffc459c1889a666e
Denis Gruia <denis.gruiax@icloud.com>
2023-09-29 17:49:35 +0300

    Improving readability the utilities module.

--- test/utilities/utilities_test.ml
+++ test/utilities/utilities_test.ml
@@ -8,22 +8,27 @@ let api_default () = let* ()  = Lwt.return (check string "api_default" Base_urls
 let test_api_default switch () = 
   Lwt_switch.add_hook (Some switch) api_default; Lwt.return ();;
 
+
 let api_gcp () = let* ()  = Lwt.return (check string "api_gcp" Base_urls.api_gcp "https://api-gcp.binance.com")
   in Lwt.return ();;
 
 let test_api_gcp switch () = 
   Lwt_switch.add_hook (Some switch) api_gcp; Lwt.return ();;
 
+
 let api1 () = let* ()  = Lwt.return (check string "api1" Base_urls.api1 "https://api1.binance.com")
   in Lwt.return ();;
 
 let test_api1 switch () = 
   Lwt_switch.add_hook (Some switch) api1; Lwt.return ();;
+
+
 let api2 () = let* ()  = Lwt.return (check string "api2" Base_urls.api1 "https://api1.binance.com")
   in Lwt.return ();;
 
 let test_api2 switch () = 
   Lwt_switch.add_hook (Some switch) api2; Lwt.return ();;
+
 
 let api3 () = let* ()  = Lwt.return (check string "api3" Base_urls.api1 "https://api1.binance.com")
   in Lwt.return ();;
@@ -31,11 +36,13 @@ let api3 () = let* ()  = Lwt.return (check string "api3" Base_urls.api1 "https:/
 let test_api3 switch () = 
   Lwt_switch.add_hook (Some switch) api3; Lwt.return ();;
 
+
 let api4 () = let* ()  = Lwt.return (check string "api4" Base_urls.api1 "https://api1.binance.com")
   in Lwt.return ();;
-
+  
 let test_api4 switch () = 
   Lwt_switch.add_hook (Some switch) api4; Lwt.return ();;
+
 
 let suite () = "Utilities endpoints", [
     Alcotest_lwt.test_case "Test of api_default url." `Quick test_api_default;
