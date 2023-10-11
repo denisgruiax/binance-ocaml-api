d52120582a9154d8f6cea6ad8d3ff9e98ee61bb7
Denis Gruia <denis.gruiax@icloud.com>
2023-10-11 18:54:24 +0300

    Refactoring the margin account borrow module.

--- lib/margin_account/margin_account_borrow/margin_account_borrow.ml
+++ lib/margin_account/margin_account_borrow/margin_account_borrow.ml
@@ -6,22 +6,17 @@ module type Parameters = sig
   val api_key : string
   val secret_key : string 
   val asset : Symbol.t
-  val is_isolated : bool
-  val symbol : Symbol.t
-  val amount : float
   val recv_window : int
 end
 
 module type Margin_account_borrow' = sig
-  val make_borrow : unit -> int Lwt.t
+  val borrow : float -> Wallet_transfer_direction.t -> int option Lwt.t
+  val isolated_borrow : Symbol.t -> float -> Wallet_transfer_direction.t -> int option Lwt.t
 end
 
 module Make(P : Parameters) : Margin_account_borrow' = struct
   let parameters = let open P in [
       ("asset", Symbol.wrap asset);
-      ("isIsolated", Binance_bool.wrap is_isolated);
-      ("symbol", Symbol.wrap symbol);
-      ("amount", string_of_float amount);
       ("recvWindows", string_of_int recv_window)
     ];;
 
@@ -30,7 +25,24 @@ module Make(P : Parameters) : Margin_account_borrow' = struct
   let headers = Requests.create_header P.api_key;;
 
   let get_data json  = json >>= fun json' -> 
-    Lwt.return (Ezjsonm.(find json' ["id"] |> get_float |> int_of_float));;
+    Lwt.return (Some (Ezjsonm.(find json' ["id"] |> get_float |> int_of_float)));;
 
-  let make_borrow () = let json = Requests.post (Uri.of_string endpoint) in get_data (json headers);;
+  let borrow amount type_of_transfer = 
+    let parameters = parameters @ [
+        ("amount", string_of_float amount);
+        ("type", Wallet_transfer_direction.wrap type_of_transfer);
+        ("isIsolated", Binance_bool.wrap false)
+      ] in
+    let url = Url.build_signed P.url "/sapi/v1/margin/transfer" parameters P.secret_key in
+    let json = Requests.post (Uri.of_string url) in get_data (json headers);;
+
+  let isolated_borrow symbol amount type_of_transfer = 
+    let parameters = parameters @ [
+        ("symbol", Symbol.wrap symbol);
+        ("amount", string_of_float amount);
+        ("type", Wallet_transfer_direction.wrap type_of_transfer);
+        ("isIsolated", Binance_bool.wrap true)
+      ] in
+    let url = Url.build_signed P.url "/sapi/v1/margin/transfer" parameters P.secret_key in
+    let json = Requests.post (Uri.of_string url) in get_data (json headers);;
 end
