7af2b44a75c63729dc7ba9113c22901315b81beb
Denis Gruia <denis.gruiax@icloud.com>
2023-10-17 17:33:36 +0300

    Extracted Base_urls into a separate folder from Utilities.

--- lib/utilities/utilities.mli
+++ lib/utilities/utilities.mli
@@ -1,12 +1,4 @@
-module Base_urls : sig
-  val api_default : string
-  val api_gcp : string
-  val api1 : string
-  val api2 : string
-  val api3 : string
-  val api4 : string
-end
-
+module Base_urls = Base_urls
 module Crypto = Crypto
 module Data = Data
 module Requests = Requests
