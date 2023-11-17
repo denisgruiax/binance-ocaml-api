a8fe9071e6f4ce20eba64122edebc488fa207235
Denis Gruia <denis.gruiax@icloud.com>
2023-11-17 20:33:40 +0200

    Revert "Update Url module to work with option variables."
    This reverts commit d982679040aef11f74a667ffb25f933affd65fb4.

--- lib/utilities/url/url.ml
+++ lib/utilities/url/url.ml
@@ -1,11 +1,15 @@
 let timestamp () = string_of_int(int_of_float ((Unix.gettimeofday ()) *. 1000.0));;
 
-let extract_pairs parameters= List.fold_right (fun elt res -> if Option.is_some elt then Option.get elt :: res else res) parameters [];;
+let check_value value = match value with
+  |"0" -> false
+  |"0.0" -> false
+  |"" -> false
+  |_ -> true;;
 
-let create_payload parameters = let res = extract_pairs parameters in match res with
+let create_payload = function
   |[] -> ""
   |[(key, value)] -> key ^ "=" ^ value
-  |(key, value) :: tail -> key ^ "=" ^ value ^ List.fold_right (fun (key, value) res -> ("&" ^ key ^ "=" ^ value ^ res)) tail "";;
+  |(key, value) :: tail -> key ^ "=" ^ value ^ List.fold_right (fun (key, value) res -> if (check_value value) then ("&" ^ key ^ "=" ^ value ^ res) else res) tail "";;
 
 let build_public url endpoint parameters_with_keys =
   let payload = create_payload parameters_with_keys  
@@ -13,9 +17,9 @@ let build_public url endpoint parameters_with_keys =
 
 let build_signed url endpoint parameters_with_keys secret_key =
   let current_timestamp = timestamp ()
-  in let payload = create_payload (parameters_with_keys @ [Some ("timestamp", current_timestamp)])
+  in let payload = create_payload (parameters_with_keys @ ["timestamp", current_timestamp])
   in String.concat "" [url; endpoint; "?"; payload; "&signature="; (Crypto.create_signature payload secret_key)];;
 
-let check_limit minimum maximum default = function
+let check_limit minimum maximum default= function
   |limit when limit >= minimum && limit <= maximum -> limit
   |_-> default;;
--- lib/utilities/url/url.mli
+++ lib/utilities/url/url.mli
@@ -1,5 +1,5 @@
 val timestamp : unit -> string
-val create_payload : (string * string) option list -> string
-val build_public : string -> string -> (string * string) option list -> string
-val build_signed : string -> string -> (string * string) option list -> string -> string
+val create_payload : (string * string) list -> string
+val build_public : string -> string -> (string * string) list -> string
+val build_signed : string -> string -> (string * string) list -> string -> string
 val check_limit : int -> int -> int -> int -> int
