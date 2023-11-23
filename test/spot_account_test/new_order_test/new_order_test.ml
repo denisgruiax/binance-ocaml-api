273d122a56e3e52cd8d52f7b460acc360f70979c
Denis Gruia <denis.gruiax@icloud.com>
2023-11-23 15:20:28 +0200

    A simple unit test have been added for New_order module under Spot_account module.

--- test/spot_account_test/new_order_test/new_order_test.ml
+++ test/spot_account_test/new_order_test/new_order_test.ml
@@ -1,30 +1,27 @@
 open Binance_ocaml_api.Spot_account;;
 open Lwt.Syntax;;
-open Utilities;;
 open Variants;;
 
-let base_url = "https://testnet.binance.vision/api";;
+let api_key = "api_key";;
+let secret_key = "secret_key";;
+let base_url = Utilities.Base_urls.api_default;;
 let endpoint = "/api/v3/order";;
-let api_key =  "";;
-let secret_key =  "";;
+
 let parameters = [
   ("symbol", "BTCUSDT");
-  ("side", Order_side.wrap BUY);
+  ("side", Order_side.wrap SELL);
   ("type", Order_types.wrap MARKET);
-  ("quantity", "1.2")
+  ("quantity", "0.1");
+  ("newOrderRespType", Order_response.(wrap ACK))
 ]
 
 let send_order () = 
-  let* response = New_order.send ~base_url:base_url ~endpoint:endpoint ~api_key:api_key ~secret_key:secret_key ~order_response:ACK ~parameters:parameters in
-
-
-  let order_response_size () = let* json = BitcoinOrder.place_order () 
-    in let* response = Lwt.return(Ezjsonm.to_string json) 
-    in Alcotest.(check bool "Order response length" ((String.length response > 10)) true);
-    Lwt.return ();;
+  let* response = New_order.send ~base_url:base_url ~endpoint:endpoint ~api_key:api_key ~secret_key:secret_key ~parameters:parameters in
+  let* () = Records.Response.print response in
+  Lwt.return ();;
 
 let test_order_response_size switch () = 
-  Lwt_switch.add_hook (Some switch) order_response_size;Lwt.return ();;
+  Lwt_switch.add_hook (Some switch) send_order; Lwt.return ();;
 
 let suite () = "New order", [
     Alcotest_lwt.(test_case "New order of buy test on BTC/USDT pair." `Quick test_order_response_size);
