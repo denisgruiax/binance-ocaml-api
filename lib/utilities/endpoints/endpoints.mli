f499c36c694d4c4b33a402635bafae45e1f21b26
Denis Gruia <denis.gruiax@icloud.com>
2023-11-23 20:04:35 +0200

    Added more endpoints to market data enpoints module

--- lib/utilities/endpoints/endpoints.mli
+++ lib/utilities/endpoints/endpoints.mli
@@ -1,3 +1,10 @@
 module Market_data : sig
+  val current_average_price : string
+  val klines : string
+  val old_trade_lookup : string
+  val order_book : string
+  val recent_trade_list : string
   val server_time : string
+  val symbol_price_ticker : string
+  val test_connectivity : string
 end
