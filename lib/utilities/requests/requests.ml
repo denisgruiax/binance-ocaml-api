3ee8c5509e8e56c0257e4f5d3c21b1060b6e54af
Denis Gruia <denis.gruiax@icloud.com>
2023-11-24 14:56:43 +0200

    Addidng white space for better readability.

--- lib/utilities/requests/requests.ml
+++ lib/utilities/requests/requests.ml
@@ -6,26 +6,26 @@ let get url =
   let* uri = Lwt.return (Uri.of_string url) 
   in let* (_, body) = Cohttp_lwt_unix.Client.get uri
   in let* body_string = Cohttp_lwt.Body.to_string body
-  in let* json = Lwt.return(Ezjsonm.from_string body_string)
+  in let* json = Lwt.return (Ezjsonm.from_string body_string)
   in Lwt.return json;;
 
 let get_signed url headers =
   let* uri = Lwt.return (Uri.of_string url)
   in let* (_, body) = Cohttp_lwt_unix.Client.get uri ~headers
   in let* body_string = Cohttp_lwt.Body.to_string body
-  in let* json = Lwt.return(Ezjsonm.from_string body_string)
+  in let* json = Lwt.return (Ezjsonm.from_string body_string)
   in Lwt.return json;;
 
 let post url headers = 
   let* uri = Lwt.return (Uri.of_string url)
   in let* (_, body) = Cohttp_lwt_unix.Client.post uri ~headers ~body:(Cohttp_lwt.Body.empty)
   in let* body_string = Cohttp_lwt.Body.to_string body
-  in let* json = Lwt.return(Ezjsonm.from_string body_string)
+  in let* json = Lwt.return (Ezjsonm.from_string body_string)
   in Lwt.return json;;
 
 let delete url headers = 
   let* uri = Lwt.return (Uri.of_string url)
   in let* (_, body) = Cohttp_lwt_unix.Client.delete uri ~headers ~body:(Cohttp_lwt.Body.empty)
   in let* body_string = Cohttp_lwt.Body.to_string body
-  in let* json = Lwt.return(Ezjsonm.from_string body_string)
+  in let* json = Lwt.return (Ezjsonm.from_string body_string)
   in Lwt.return json;;
