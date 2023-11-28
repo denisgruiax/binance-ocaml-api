db930832db41008fecebe693a03e6f0f6714378e
Denis Gruia <denis.gruiax@icloud.com>
2023-11-28 20:18:08 +0200

    Making Account module to return the type ('a, 'b) result from Stdlib instead of returning a option.

--- lib/spot_account/account/account.mli
+++ lib/spot_account/account/account.mli
@@ -16,7 +16,7 @@ type account = {
   taker_commision : Decimal.t;
   buyer_commission : Decimal.t;
   seller_commission : Decimal.t;
-  commission_rates : commission_rate option;
+  commission_rates : (commission_rate, Error_code.t) result;
   can_trade : bool;
   can_withdraw : bool;
   can_deposit : bool;
@@ -25,11 +25,11 @@ type account = {
   prevent_sor : bool;
   update_time : Decimal.t;
   account_type : string;
-  balances : balance option list;
+  balances : (balance, Error_code.t) result list;
   permissions : string list;
   uid : Decimal.t
 };;
 
-val get_account : string -> string -> string -> string -> (string * string) list -> account option Lwt.t;;
+val get_account : string -> string -> string -> string -> (string * string) list -> (account, Error_code.t) result Lwt.t;;
 
-val print_account : account option -> unit Lwt.t
+val print_account : (account, Error_code.t) result -> unit Lwt.t
