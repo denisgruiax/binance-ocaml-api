0501df894d8ebde05bfad27554fac6b4c5435415
Denis Gruia <denis.gruiax@icloud.com>
2023-11-28 20:10:03 +0200

    Making Test_connectivity to return the type ('a, 'b) result from Stdlib instead of returning a option.

--- lib/market_data_endpoints/test_connectivity/test_connectivity.ml
+++ lib/market_data_endpoints/test_connectivity/test_connectivity.ml
@@ -1,7 +1,12 @@
 open Utilities;;
 open Lwt.Infix;;
+
+let get_data = function
+  |`O[] -> Ok "{}"
+  |error -> Error (Error_code.get error);;
+
 let json_to_string json = 
-  json >>= fun json' -> Lwt.return (Ezjsonm.to_string json');;
+  json >>= fun json' -> Lwt.return (get_data json');;
 
 let get ~base_url:base_url ~endpoint:endpoint = let url = String.concat "" [base_url; endpoint] in
   json_to_string (Requests.get url);;
